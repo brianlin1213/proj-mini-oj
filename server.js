03c97f69be61fb54f0d180e3d30c2decc3b45729
HakLin <brianlin1122@gmail.com>
2026-07-02 15:12:56 +0800

    change dev port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { spawn } = require("child_process");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.json({ limit: "2mb" }));
 app.use(express.static("public"));
